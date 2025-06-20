7660d63416d5b2c6df19e2431b96b877bf07353c
khx <106915370+dpejoh@users.noreply.github.com>
2025-06-20 19:08:15 +0100

    - enhancing ui_print context - adding module.zip file

--- Module/action.sh
+++ Module/action.sh
@@ -10,21 +10,22 @@ ui_print() {
   echo "$1"
 }
 
-ui_print "Checking latest available keybox..."
+ui_print "- Checking latest available keybox..."
 
 VERSION=$(curl -fsSL "$VERSION_URL")
 # If curl fails, fallback message
 if [ -z "$VERSION" ]; then
-  ui_print "Failed to fetch version info."
+  ui_print "- Failed to fetch version info."
 else
-  ui_print "$VERSION version available"
+  ui_print "- $VERSION version available."
 fi
   
-ui_print "Downloading and overriding keybox.xml..."
+ui_print "- Downloading and overriding keybox.xml..."
 if command -v curl >/dev/null 2>&1; then
-  curl -fsSL "$REMOTE_URL" -o "$TARGET_FILE" && ui_print "keybox.xml successfully updated."
+  curl -fsSL "$REMOTE_URL" -o "$TARGET_FILE" && ui_print "- keybox.xml successfully updated."
 elif command -v wget >/dev/null 2>&1; then
-  wget -qO "$TARGET_FILE" "$REMOTE_URL" && ui_print "keybox.xml successfully updated."
+  wget -qO "$TARGET_FILE" "$REMOTE_URL" && ui_print "- keybox.xml successfully updated."
 else
-  ui_print "Error: curl or wget not available. Cannot fetch remote keybox."
+  ui_print "- Error: curl or wget not available."
+  ui_print "- Cannot fetch remote keybox"
 fi
--- Module/customize.sh
+++ Module/customize.sh
@@ -9,34 +9,43 @@ ui_print() {
   echo "$1"
 }
 
+ui_print ""
+ui_print "*********************************"
+ui_print "*****Yuri Keybox Installer*******"
+ui_print "*********************************"
+ui_print ""
+
 override_keybox() {
-  ui_print "Downloading and overriding keybox.xml..."
+  ui_print "- Downloading and overriding keybox.xml..."
   if command -v curl >/dev/null 2>&1; then
-    curl -fsSL "$REMOTE_URL" -o "$TARGET_FILE" && ui_print "keybox.xml successfully updated."
+    curl -fsSL "$REMOTE_URL" -o "$TARGET_FILE" && ui_print "- keybox.xml successfully updated."
   elif command -v wget >/dev/null 2>&1; then
-    wget -qO "$TARGET_FILE" "$REMOTE_URL" && ui_print "keybox.xml successfully updated."
+    wget -qO "$TARGET_FILE" "$REMOTE_URL" && ui_print "- keybox.xml successfully updated."
   else
-    ui_print "Error: curl or wget not available. Cannot fetch remote keybox."
+    ui_print "- Error: curl or wget not available."
+    ui_print "- Cannot fetch remote keybox."
   fi
 }
 
 # Start logic
-ui_print "Checking if there is an existing keybox..."
+ui_print "- Checking if there is an existing keybox..."
 
 mkdir -p "$TRICKY_DIR"
 
 if [ -f "$TARGET_FILE" ]; then
   if grep -q "yuriiroot" "$TARGET_FILE"; then
-    ui_print "Existing keybox was made by Yuri. Overriding it."
+    ui_print "- Existing Yuri Keybox found."
+    ui_print "- Overriding it..."
     override_keybox
   else
-    ui_print "A keybox already exists but it wasn't made by yuriiroot."
-    ui_print "Creating a backup and overriding with the new one..."
+    ui_print "- Existing keybox not by Yuri."
+    ui_print "- Creating a backup..."
     mv "$TARGET_FILE" "$BACKUP_FILE"
+    ui_print "- Overriding with the new one..."
     override_keybox
   fi
 else
-  ui_print "No keybox found. Creating a new one."
+  ui_print "- No keybox found. Creating a new one."
   touch "$TARGET_FILE"
   override_keybox
 fi
