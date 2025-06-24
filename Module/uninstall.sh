7cf7c771ea202265fb718926d818bad61142b253
ＺＧＸ０８９ッ <159061718+ZG089@users.noreply.github.com>
2025-06-24 15:58:55 +0300

    module_uninstall: remove unimportant code

--- Module/uninstall.sh
+++ Module/uninstall.sh
@@ -11,24 +11,12 @@ ui_print() {
 backup () {
 if [ -f "$BACKUP_FILE" ]; then
   rm -f "$TARGET_FILE"
-  ui_print "- Removed Yuri Keybox."
   mv "$BACKUP_FILE" "$TARGET_FILE"
-  ui_print "- Restored previous keybox."
-else
-  ui_print "- No backup found to restore."
 fi
 }
 
-ui_print "- Uninstalling Yuri keybox..."
-
-
 if [ -f "$TARGET_FILE" ]; then
   if grep -q "yuriiroot" "$TARGET_FILE"; then
-    ui_print "- Existing Yuri Keybox found."
     backup
-    ui_print "- Uninstall complete."
-  else
-    ui_print "- Existing keybox not by Yuri."
-    ui_print "- Nothing Done."
   fi
 fi
