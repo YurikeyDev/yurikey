df261612f0d6d2a9a5be1ca38745605640d5a328
ZG089 <159061718+ZG089@users.noreply.github.com>
2025-07-05 10:59:27 +0300

    module_gmskill: Improve process kill code, add a new gms process.

--- Module/Yuri/kill_google_process.sh
+++ Module/Yuri/kill_google_process.sh
@@ -9,11 +9,10 @@ log_message "Start"
 
 # Writing
 log_message "Writing"
-PKGS="com.android.vending"
+PKGS="com.android.vending com.google.android.gms.unstable"
 for pkg in $PKGS; do
-    am force-stop "$pkg"     >/dev/null 2>&1
-    pm clear "$pkg"          >/dev/null 2>&1
+    pid=$(pidof "$pkg")
+    [ -n "$pid" ] && kill -9 "$pid"
 done
-
 # Finish
 log_message "Finish"
