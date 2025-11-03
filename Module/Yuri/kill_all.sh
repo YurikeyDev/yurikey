16e616771631e93c4820d25ad7dab5912cd31a10
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2025-11-03 21:41:58 +0700

    Stop clear Google Play Services data

--- Module/Yuri/kill_all.sh
+++ Module/Yuri/kill_all.sh
@@ -9,7 +9,7 @@ log_message "Start"
 
 # Writing
 log_message "Writing"
-PKGS="com.android.vending com.google.android.gsf com.google.android.gms com.google.android.contactkeys com.google.android.ims com.google.android.safetycore com.google.android.apps.walletnfcrel com.google.android.apps.nbu.paisa.user com.zhenxi.hunter com.reveny.nativecheck io.github.vvb2060.keyattestation io.github.vvb2060.mahoshojo icu.nullptr.nativetest com.android.nativetest io.liankong.riskdetector me.garfieldhan.holmes luna.safe.luna com.zhenxi.hunter gr.nikolasspyr.integritycheck com.youhu.laifu"
+PKGS="com.android.vending com.google.android.gsf com.google.android.contactkeys com.google.android.ims com.google.android.safetycore com.google.android.apps.walletnfcrel com.google.android.apps.nbu.paisa.user com.zhenxi.hunter com.reveny.nativecheck io.github.vvb2060.keyattestation io.github.vvb2060.mahoshojo icu.nullptr.nativetest com.android.nativetest io.liankong.riskdetector me.garfieldhan.holmes luna.safe.luna com.zhenxi.hunter gr.nikolasspyr.integritycheck com.youhu.laifu"
 
 for pkg in $PKGS; do
     if ! am force-stop "$pkg" >/dev/null 2>&1; then
