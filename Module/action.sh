2dbd87b1b240035c4ad37a14a7b82db23438f295
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2025-12-20 00:44:42 +0700

    Change print line

--- Module/action.sh
+++ Module/action.sh
@@ -6,16 +6,23 @@ unset ASH_STANDALONE
 
 for SCRIPT in \
   "kill_google_process.sh" \
-  "yuri_keybox.sh" \
   "target_txt.sh" \
   "security_patch.sh" \
-  "boot_hash.sh"
+  "boot_hash.sh" \
+  "clear_trace.sh" \
+  "yuri_keybox.sh"
 do
   if ! sh "$MODPATH/Yuri/$SCRIPT"; then
     echo "- Error: $SCRIPT failed. Aborting."
     exit 1
+  elif ! sh "$MODPATH/Yuri/yuri_keybox.sh"; then
+    log_message "- Cannot fetch remote keybox. Aborting"
+    log_message "- Tip: You can install a working BusyBox with network tools from:"
+    log_message "- https://mmrl.dev/repository/grdoglgmr/busybox-ndk"
+    exit 1
   fi
 done
+
 
 if [ -f /data/adb/modules_update/Yurikey/webroot/common/device-info.sh ]; then
   sh /data/adb/modules_update/Yurikey/webroot/common/device-info.sh
