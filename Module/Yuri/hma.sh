1317ac5fbad592d521a9b58eb64a2d761b7eed3a
Hzz Monet <hzzmonet7@gmail.com>
2025-12-20 13:18:48 +0700

    Add update hma oss config script

---
+++ Module/Yuri/hma.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+mkdir -p /data/user/0/org.frknkrc44.hma_oss/files
+curl -L "https://raw.githubusercontent.com/hzzmonetvn/yurikey/refs/heads/main/config.json" -o /data/user/0/org.frknkrc44.hma_oss/files/config.json
+chmod 777 /data/user/0/org.frknkrc44.hma_oss/files/config.json
+chown u0_a0:u0_a0 /data/user/0/org.frknkrc44.hma_oss/files/config.json
